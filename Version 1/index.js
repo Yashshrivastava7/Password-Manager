98bab873c6598e917787cd088bf45e02f625435d
Yash Shrivastava <yash.shrivastava77@gmail.com>
2022-09-02 22:43:04 +0530

    Added Joi for object simplification

--- Version 1/index.js
+++ Version 1/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
-const PORT = 8080;
+const Joi = require('joi')
+const PORT = process.env.PORT || 8080;
+app.use(express.json());
 
 app.get('/',(req,res) => {
     res.status(200).send("Endpoint Working")
 });
 
-app.listen(PORT, () => console.log('Listing at port 8080'));
-
+app.listen(PORT, () => console.log(`Listing at port ${PORT}`));
